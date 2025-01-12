96fc40581243989ae2700edf9f1be44cbcbbd55a
Zach Chen <zachxdc@gmail.com>
2025-01-12 16:16:52 +1100

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+import React from "react";
+
+const rootElement = document.getElementById("root");
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
